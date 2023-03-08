1e41c4dcd82777634e36ff621c42a24af5d4a632
pparksso <thgml5143@hanmail.net>
2023-03-08 10:57:49 +0900

    first commit

---
+++ components/TheFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer>footer</footer>
+</template>
+<script lang="ts"></script>
+<style lang="scss" scoped></style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,5 @@
+<template>
+  <header>header</header>
+</template>
+<script lang="ts"></script>
+<style lang="scss" scoped></style>
